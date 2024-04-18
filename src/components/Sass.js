f7170ce65b4f61aca85cddb23217cc467cc4dec0
SamyukthaVajja <samyuktha.vajja@senecaglobal.com>
2024-04-18 20:36:53 +0530

    first commit

---
+++ src/components/Sass.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './main.scss';
+
+const Sass = () => {
+  return (
+    <div className='main-header'>
+        <h1>
+            Hi This is SASS 
+        </h1>
+        <p>This is scss</p>
+    </div>
+  )
+}
+
+export default Sass
